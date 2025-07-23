4ea877354cdfcf8630d2527be92a9f04bb615610
Charles Lopez <charlessamuell50@gmail.com>
2025-07-24 02:01:51 +0800

    added input validation

--- python_practices/mult_table.py
+++ python_practices/mult_table.py
@@ -3,14 +3,24 @@ def main ():
     while True:
         try:
             num = int(input("What multiplication table do you want to generate?: "))
-            print(f"Multiplication table of {num}")
+            limit = int(input(f"{num} x up to? "))
+            print(f"Multiplication table of {num} up to {limit}")
         except ValueError:
             print("Please enter an integer")
             continue
-
-        for i in range(1,10 + 1,1):
+        
+        for i in range(1, limit + 1):
             print(f"{num} x {i} = {num * i}")
-    
+        while True:
+            choice = (input("Do you want to try another number? (yes/y or no/n) ")).lower()
+            if choice in ["y", "yes"]:
+                break
+            elif choice in ["n", "no"]:
+                print("Thank you! Exiting program...")
+                exit()
 
+            else:
+                print("Invalid input! please enter \"yes/y\" or \"no/n\"")
+            continue
 if __name__ == "__main__":
     main()
