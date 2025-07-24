278078ccb1f9acb1e25c502db9942855224e5717
Charles Lopez <charlessamuell50@gmail.com>
2025-07-25 00:57:41 +0800

    .

--- python_practices/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-choice = input("Do you want to play again? (y/n): ").lower()
-            if choice in ['y']:
-                break
-            elif choice in ['n']:
-                print("Thank you for playing")
-                return
-            else:
-                print("Please enter a valid input y or n!")
-                continue
