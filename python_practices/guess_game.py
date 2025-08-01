1dde753dcd33d8249c166ac715026cf84a1951ba
Charles Lopez <charlessamuell50@gmail.com>
2025-08-01 11:26:19 +0800

    converted into a function (guess())

--- python_practices/guess_game.py
+++ python_practices/guess_game.py
@@ -14,6 +14,29 @@ def repeat():
             else:
                 print("Please enter a valid input y or n!")
                 continue
+def guess(attempts = 0, max_attempts = 10):
+    print("\nGuess the lucky number from 1-100!\n")
+    print(f"Note: You only have {max_attempts} attempts, Goodluck!")
+    target = random.randint(1,100)
+    while attempts < max_attempts:
+            try:
+                    guess = int(input(f"Attempt {attempts + 1}/{max_attempts} Guess: "))
+                    attempts += 1
+
+
+                    if guess == target:
+                        print("You have won 1000 dollars!\n")
+                        break
+                    elif guess > target:
+                        print("Too High! Try another number\n")
+                    else:
+                        print("Too Low! Try another number\n")
+            except ValueError:
+                    print("Please enter a valid number!")
+
+    else: 
+        print(f"You have ran out of attempts! The lucky number was {target}")
+        
 def main():
 
     attempts = 0
@@ -28,28 +51,7 @@ def main():
                 time.sleep(0.4)
         print("\r" + ' ' * 20 + "\r" , end = '')
 
-        print("\nGuess the lucky number from 1-100!\n")
-        print(f"Note: You only have {max_attempts} attempts, Goodluck!")
-        target = random.randint(1,100)
-        while attempts < max_attempts:
-            try:
-                guess = int(input(f"Attempt {attempts + 1}/{max_attempts} Guess: "))
-                attempts += 1
-
-
-                if guess == target:
-                    print("You have won 1000 dollars!\n")
-                    break
-                elif guess > target:
-                    print("Too High! Try another number\n")
-                else:
-                    print("Too Low! Try another number\n")
-            except ValueError:
-                print("Please enter a valid number!")
-
-        else: 
-            print(f"You have ran out of attempts! The lucky number was {target}")
-        
+        guess(attempts, max_attempts)
         repeat()
         
 
