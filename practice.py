6b1b2f7d14ff9a9a559f750d0e90d7ad83cf6b7e
Charles Lopez <charlessamuell50@gmail.com>
2025-08-06 15:27:04 +0800

    tyl

--- practice.py
+++ practice.py
@@ -47,9 +47,19 @@ string = "python console"
 #for i in goat:
  #   print(f"{i}")
 
-sum = 0
-for number in range(1,11):
-    print(f"number: {number}")
-    sum += number
-print(f"sum is: {sum}")
+# sum = 0
+# for number in range(1,11):
+#     print(f"number: {number}")
+#     sum += number
+# print(f"sum is: {sum}")
 
+nums = [1, 2, 3, 4, 5, 6]
+evens = []
+for x in nums:
+    if x % 2 == 0:
+        evens.append(x)
+print(evens)  # [2, 4, 6]
+
+my_set = {1,2,3,4,5}
+my_set.add(7)
+print(my_set)
